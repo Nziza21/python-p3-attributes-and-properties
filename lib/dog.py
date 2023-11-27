22bbda78325e1d59f4b662b007289adf2a2d55f5
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-11-27 22:05:39 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,13 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="", breed=""):
+        if not name or not isinstance(name, str) or not 1 <= len(name) <= 25:
+            print("Name must be a string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+        if breed and breed not in APPROVED_BREEDS:
+            print("Breed must be in the list of approved breeds.")
+        else:
+            self.breed = breed
